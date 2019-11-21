911cf2420dc08be9c0851764f0c31ad2ea86af32
Krzysztof <krzysiek71198@gmail.com>
2019-11-21 22:47:32 +0100

    Wstępna aplikacja farmy

---
+++ lab1/KrzysztofJopekLab1Zadanie/Farm.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace KrzysztofJopekLab1Zadanie
+{
+    partial class Farm
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Farm));
+            this.sheep1 = new System.Windows.Forms.PictureBox();
+            this.sheep2 = new System.Windows.Forms.PictureBox();
+            this.sheep3 = new System.Windows.Forms.PictureBox();
+            this.sheep4 = new System.Windows.Forms.PictureBox();
+            this.sheep5 = new System.Windows.Forms.PictureBox();
+            this.sheep6 = new System.Windows.Forms.PictureBox();
+            this.sheep7 = new System.Windows.Forms.PictureBox();
+            this.sheep8 = new System.Windows.Forms.PictureBox();
+            this.wolf = new System.Windows.Forms.PictureBox();
+            this.dog = new System.Windows.Forms.PictureBox();
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.dogButton = new System.Windows.Forms.Button();
+            this.buttonTime2x = new System.Windows.Forms.Button();
+            this.dayNightLabel = new System.Windows.Forms.Label();
+            this.dogInHouseButton = new System.Windows.Forms.Button();
+            this.sheepCounterLabelName = new System.Windows.Forms.Label();
+            this.sheepCounterLabel = new System.Windows.Forms.Label();
+            this.rulesButton = new System.Windows.Forms.Button();
+            this.numberOfDayLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.sheep1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sheep2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sheep3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sheep4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sheep5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sheep6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sheep7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sheep8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wolf)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dog)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // sheep1
+            // 
+            this.sheep1.Image = ((System.Drawing.Image)(resources.GetObject("sheep1.Image")));
+            this.sheep1.Location = new System.Drawing.Point(170, 92);
+            this.sheep1.Margin = new System.Windows.Forms.Padding(2);
+            this.sheep1.Name = "sheep1";
+            this.sheep1.Size = new System.Drawing.Size(131, 122);
+            this.sheep1.TabIndex = 0;
+            this.sheep1.TabStop = false;
+            // 
+            // sheep2
+            // 
+            this.sheep2.Image = ((System.Drawing.Image)(resources.GetObject("sheep2.Image")));
+            this.sheep2.Location = new System.Drawing.Point(319, 92);
+            this.sheep2.Margin = new System.Windows.Forms.Padding(2);
+            this.sheep2.Name = "sheep2";
+            this.sheep2.Size = new System.Drawing.Size(131, 122);
+            this.sheep2.TabIndex = 1;
+            this.sheep2.TabStop = false;
+            // 
+            // sheep3
+            // 
+            this.sheep3.Image = ((System.Drawing.Image)(resources.GetObject("sheep3.Image")));
+            this.sheep3.Location = new System.Drawing.Point(475, 92);
+            this.sheep3.Margin = new System.Windows.Forms.Padding(2);
+            this.sheep3.Name = "sheep3";
+            this.sheep3.Size = new System.Drawing.Size(131, 122);
+            this.sheep3.TabIndex = 2;
+            this.sheep3.TabStop = false;
+            // 
+            // sheep4
+            // 
+            this.sheep4.Image = ((System.Drawing.Image)(resources.GetObject("sheep4.Image")));
+            this.sheep4.Location = new System.Drawing.Point(633, 92);
+            this.sheep4.Margin = new System.Windows.Forms.Padding(2);
+            this.sheep4.Name = "sheep4";
+            this.sheep4.Size = new System.Drawing.Size(131, 122);
+            this.sheep4.TabIndex = 3;
+            this.sheep4.TabStop = false;
+            // 
+            // sheep5
+            // 
+            this.sheep5.Image = ((System.Drawing.Image)(resources.GetObject("sheep5.Image")));
+            this.sheep5.Location = new System.Drawing.Point(170, 234);
+            this.sheep5.Margin = new System.Windows.Forms.Padding(2);
+            this.sheep5.Name = "sheep5";
+            this.sheep5.Size = new System.Drawing.Size(131, 122);
+            this.sheep5.TabIndex = 4;
+            this.sheep5.TabStop = false;
+            // 
+            // sheep6
+            // 
+            this.sheep6.Image = ((System.Drawing.Image)(resources.GetObject("sheep6.Image")));
+            this.sheep6.Location = new System.Drawing.Point(319, 234);
+            this.sheep6.Margin = new System.Windows.Forms.Padding(2);
+            this.sheep6.Name = "sheep6";
+            this.sheep6.Size = new System.Drawing.Size(131, 122);
+            this.sheep6.TabIndex = 5;
+            this.sheep6.TabStop = false;
+            // 
+            // sheep7
+            // 
+            this.sheep7.Image = ((System.Drawing.Image)(resources.GetObject("sheep7.Image")));
+            this.sheep7.Location = new System.Drawing.Point(475, 234);
+            this.sheep7.Margin = new System.Windows.Forms.Padding(2);
+            this.sheep7.Name = "sheep7";
+            this.sheep7.Size = new System.Drawing.Size(131, 122);
+            this.sheep7.TabIndex = 6;
+            this.sheep7.TabStop = false;
+            // 
+            // sheep8
+            // 
+            this.sheep8.Image = ((System.Drawing.Image)(resources.GetObject("sheep8.Image")));
+            this.sheep8.Location = new System.Drawing.Point(633, 234);
+            this.sheep8.Margin = new System.Windows.Forms.Padding(2);
+            this.sheep8.Name = "sheep8";
+            this.sheep8.Size = new System.Drawing.Size(131, 122);
+            this.sheep8.TabIndex = 7;
+            this.sheep8.TabStop = false;
+            // 
+            // wolf
+            // 
+            this.wolf.Image = ((System.Drawing.Image)(resources.GetObject("wolf.Image")));
+            this.wolf.Location = new System.Drawing.Point(797, 39);
+            this.wolf.Margin = new System.Windows.Forms.Padding(2);
+            this.wolf.Name = "wolf";
+            this.wolf.Size = new System.Drawing.Size(131, 122);
+            this.wolf.TabIndex = 8;
+            this.wolf.TabStop = false;
+            // 
+            // dog
+            // 
+            this.dog.Image = ((System.Drawing.Image)(resources.GetObject("dog.Image")));
+            this.dog.Location = new System.Drawing.Point(18, 39);
+            this.dog.Margin = new System.Windows.Forms.Padding(2);
+            this.dog.Name = "dog";
+            this.dog.Size = new System.Drawing.Size(131, 122);
+            this.dog.TabIndex = 9;
+            this.dog.TabStop = false;
+            // 
+            // Timer
+            // 
+            this.Timer.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.timeLabel.Location = new System.Drawing.Point(821, 201);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(74, 31);
+            this.timeLabel.TabIndex = 10;
+            this.timeLabel.Text = "Time";
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(319, 385);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(131, 47);
+            this.startButton.TabIndex = 11;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // dogButton
+            // 
+            this.dogButton.Location = new System.Drawing.Point(18, 181);
+            this.dogButton.Name = "dogButton";
+            this.dogButton.Size = new System.Drawing.Size(131, 51);
+            this.dogButton.TabIndex = 12;
+            this.dogButton.Text = "Dog in field";
+            this.dogButton.UseVisualStyleBackColor = true;
+            this.dogButton.Click += new System.EventHandler(this.dogButton_Click);
+            // 
+            // buttonTime2x
+            // 
+            this.buttonTime2x.Location = new System.Drawing.Point(827, 252);
+            this.buttonTime2x.Name = "buttonTime2x";
+            this.buttonTime2x.Size = new System.Drawing.Size(68, 31);
+            this.buttonTime2x.TabIndex = 13;
+            this.buttonTime2x.Text = "Time 4x";
+            this.buttonTime2x.UseVisualStyleBackColor = true;
+            this.buttonTime2x.Click += new System.EventHandler(this.buttonTime2x_Click);
+            // 
+            // dayNightLabel
+            // 
+            this.dayNightLabel.AutoSize = true;
+            this.dayNightLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.dayNightLabel.Location = new System.Drawing.Point(202, 18);
+            this.dayNightLabel.Name = "dayNightLabel";
+            this.dayNightLabel.Size = new System.Drawing.Size(99, 51);
+            this.dayNightLabel.TabIndex = 14;
+            this.dayNightLabel.Text = "Day";
+            // 
+            // dogInHouseButton
+            // 
+            this.dogInHouseButton.Location = new System.Drawing.Point(18, 252);
+            this.dogInHouseButton.Name = "dogInHouseButton";
+            this.dogInHouseButton.Size = new System.Drawing.Size(131, 51);
+            this.dogInHouseButton.TabIndex = 15;
+            this.dogInHouseButton.Text = "Dog in house";
+            this.dogInHouseButton.UseVisualStyleBackColor = true;
+            this.dogInHouseButton.Click += new System.EventHandler(this.dogInHouseButton_Click);
+            // 
+            // sheepCounterLabelName
+            // 
+            this.sheepCounterLabelName.AutoSize = true;
+            this.sheepCounterLabelName.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.sheepCounterLabelName.Location = new System.Drawing.Point(367, 18);
+            this.sheepCounterLabelName.Name = "sheepCounterLabelName";
+            this.sheepCounterLabelName.Size = new System.Drawing.Size(334, 51);
+            this.sheepCounterLabelName.TabIndex = 16;
+            this.sheepCounterLabelName.Text = "Sheep Counter: ";
+            // 
+            // sheepCounterLabel
+            // 
+            this.sheepCounterLabel.AutoSize = true;
+            this.sheepCounterLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.sheepCounterLabel.Location = new System.Drawing.Point(685, 18);
+            this.sheepCounterLabel.Name = "sheepCounterLabel";
+            this.sheepCounterLabel.Size = new System.Drawing.Size(70, 51);
+            this.sheepCounterLabel.TabIndex = 17;
+            this.sheepCounterLabel.Text = "24";
+            // 
+            // rulesButton
+            // 
+            this.rulesButton.Location = new System.Drawing.Point(475, 385);
+            this.rulesButton.Name = "rulesButton";
+            this.rulesButton.Size = new System.Drawing.Size(131, 47);
+            this.rulesButton.TabIndex = 18;
+            this.rulesButton.Text = "Rules";
+            this.rulesButton.UseVisualStyleBackColor = true;
+            this.rulesButton.Click += new System.EventHandler(this.rulesButton_Click);
+            // 
+            // numberOfDayLabel
+            // 
+            this.numberOfDayLabel.AutoSize = true;
+            this.numberOfDayLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.numberOfDayLabel.Location = new System.Drawing.Point(297, 18);
+            this.numberOfDayLabel.Name = "numberOfDayLabel";
+            this.numberOfDayLabel.Size = new System.Drawing.Size(46, 51);
+            this.numberOfDayLabel.TabIndex = 19;
+            this.numberOfDayLabel.Text = "1";
+            // 
+            // Farm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(959, 448);
+            this.Controls.Add(this.numberOfDayLabel);
+            this.Controls.Add(this.rulesButton);
+            this.Controls.Add(this.sheepCounterLabel);
+            this.Controls.Add(this.sheepCounterLabelName);
+            this.Controls.Add(this.dogInHouseButton);
+            this.Controls.Add(this.dayNightLabel);
+            this.Controls.Add(this.buttonTime2x);
+            this.Controls.Add(this.dogButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.timeLabel);
+            this.Controls.Add(this.dog);
+            this.Controls.Add(this.wolf);
+            this.Controls.Add(this.sheep8);
+            this.Controls.Add(this.sheep7);
+            this.Controls.Add(this.sheep6);
+            this.Controls.Add(this.sheep5);
+            this.Controls.Add(this.sheep4);
+            this.Controls.Add(this.sheep3);
+            this.Controls.Add(this.sheep2);
+            this.Controls.Add(this.sheep1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Farm";
+            this.RightToLeftLayout = true;
+            this.Text = "Farm";
+            this.Load += new System.EventHandler(this.Farm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.sheep1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sheep2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sheep3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sheep4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sheep5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sheep6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sheep7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sheep8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wolf)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dog)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox sheep1;
+        private System.Windows.Forms.PictureBox sheep2;
+        private System.Windows.Forms.PictureBox sheep3;
+        private System.Windows.Forms.PictureBox sheep4;
+        private System.Windows.Forms.PictureBox sheep5;
+        private System.Windows.Forms.PictureBox sheep6;
+        private System.Windows.Forms.PictureBox sheep7;
+        private System.Windows.Forms.PictureBox sheep8;
+        private System.Windows.Forms.PictureBox wolf;
+        private System.Windows.Forms.PictureBox dog;
+        private System.Windows.Forms.Timer Timer;
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button dogButton;
+        private System.Windows.Forms.Button buttonTime2x;
+        private System.Windows.Forms.Label dayNightLabel;
+        private System.Windows.Forms.Button dogInHouseButton;
+        private System.Windows.Forms.Label sheepCounterLabelName;
+        private System.Windows.Forms.Label sheepCounterLabel;
+        private System.Windows.Forms.Button rulesButton;
+        private System.Windows.Forms.Label numberOfDayLabel;
+    }
+}
+
---
+++ lab1/KrzysztofJopekLab1Zadanie/Farm.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KrzysztofJopekLab1Zadanie
+{
+    public partial class Farm : Form
+    {
+        /// <summary>
+        /// zmienna przechowująca obecny czas 
+        /// </summary>
+        int number = 0;
+        /// <summary>
+        /// zmienna przechowująca liczbę owiec
+        /// </summary>
+        int sheepCounter = 24;
+        /// <summary>
+        /// zmienna przechowująca stan czy wilk zjadł owce
+        /// </summary>
+        bool wolfAte = false;
+        /// <summary>
+        /// zmienna przechowująca numer dnia
+        /// </summary>
+        int numberOfDay = 1;
+        public Farm()
+        {
+            InitializeComponent();
+        }
+
+        private void Farm_Load(object sender, EventArgs e)
+        {
+            BackColor = Color.LightYellow;
+            sheep1.Visible = false;
+            sheep2.Visible = false;
+            sheep3.Visible = false;
+            sheep4.Visible = false;
+            sheep5.Visible = false;
+            sheep6.Visible = false;
+            sheep7.Visible = false;
+            sheep8.Visible = false;
+            wolf.Visible = false;
+            dog.Visible = false;
+            sheepCounterLabel.Text = sheepCounter.ToString();
+            numberOfDayLabel.Text = numberOfDay.ToString();
+
+        }
+        /// <summary>
+        /// metody wykonujące się przy każdym ticku timera
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timeLabel.Text = number.ToString();
+            sheepCounterLabel.Text = sheepCounter.ToString();
+
+            if (wolf.Visible == true && dog.Visible == false)
+            {
+                sheep1.Visible = false;
+                sheep2.Visible = false;
+                sheep3.Visible = false;
+                sheep4.Visible = false;
+                sheep5.Visible = false;
+                sheep6.Visible = false;
+                sheep7.Visible = false;
+                sheep8.Visible = false;
+                wolfAte = true;
+
+            }
+            // Jeśli ilość owiec = 0 -> KONIEC GRY
+            if (sheepCounter == 0)
+            {
+                Timer.Stop();
+                MessageBox.Show("You lost all sheeps.", "GAME OVER", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+            if( numberOfDay == 8)
+            {
+                Timer.Stop();
+                MessageBox.Show("Congratulations!", "YOU WIN", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            //Pojawianie się kolejnych owiec co 5 sekund
+            switch (number)
+            {
+                case 5:
+                    {
+                        sheep1.Visible = true;
+                        break;
+                    }
+                case 10:
+                    {
+                        sheep2.Visible = true;
+                        break;
+                    }
+                case 15:
+                    {
+                        sheep3.Visible = true;
+                        break;
+                    }
+                case 20:
+                    {
+                        sheep4.Visible = true;
+                        break;
+                    }
+                case 25:
+                    {
+                        sheep5.Visible = true;
+                        break;
+                    }
+                case 30:
+                    {
+                        sheep6.Visible = true;
+                        break;
+                    }
+                case 35:
+                    {
+                        sheep7.Visible = true;
+                        break;
+                    }
+                case 40:
+                    {
+                        sheep8.Visible = true;
+                        break;
+                    }
+                //nastanie nocy, pojawienie się wilka
+                case 50:
+                    {
+                        BackColor = Color.DarkBlue;
+                        dayNightLabel.Text = "Night";
+                        wolf.Visible = true;
+                        break;
+                    }
+                //nastanie dnia, wilka znika, dzień zaczyna się od nowa
+                case 55:
+                    {
+                        numberOfDay++;
+                        numberOfDayLabel.Text = numberOfDay.ToString();
+                        wolf.Visible = false;
+                        dayNightLabel.Text = "Day";
+                        BackColor = Color.LightYellow;
+                        number = 0;
+                        if (wolfAte == true)
+                        {
+                            sheepCounter -= 8;
+                        }
+                        break;
+                    }
+            }
+            number++;
+        }
+        /// <summary>
+        /// Przycisk uruchamiający timer
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            Timer.Interval = 400;
+            Timer.Start();
+
+        }
+        /// <summary>
+        /// Przycisk "przywołujący" psa
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void dogButton_Click(object sender, EventArgs e)
+        {
+            dog.Visible = true;
+        }
+        /// <summary>
+        /// Przycisk do przyśpieszania czasu
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonTime2x_Click(object sender, EventArgs e)
+        {
+            Timer.Interval = 100;
+        }
+        /// <summary>
+        /// Przycisk "odwołujący" psa
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void dogInHouseButton_Click(object sender, EventArgs e)
+        {
+            dog.Visible = false;
+        }
+        /// <summary>
+        /// Przycisk wyświetlający zasady gry
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void rulesButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("ZASADY GRY: \nCelem gry jest przetrwać 7 dni nie tracąc wszystkich owiec. Owce pojawiają się na pastwisu w dzień (max. 8), " +
+                "opcjonalnie moze pilnować je pies pasterski.\nPo pewnym czasie nastaje noc, kiedy zjawia się wilk. Jeżeli pies nie pilnuje owiec, wilk " +
+                "zjada wszystkie owce z pastwiska. \nNastępnie nastaje nowy dzień,  jeśli owce zostały zjedzone, uzupełniają je na pastwisku kolejne.\n" +
+                "Jeśli stracisz wszystkie owce przed upływem 7 dni, przegrywasz. \nJeśli uda Ci się przetrwać, gratulacje! Wygrałeś :)", "ZASADY GRY",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+                
+        }
+    }
+}
