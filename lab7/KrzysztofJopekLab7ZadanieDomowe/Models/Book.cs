b694ce90b6657bf5bc5783acb9662af08cd31304
Krzysztof <krzysiek71198@gmail.com>
2020-01-22 15:07:31 +0100

    add lab7 and lab 8

---
+++ lab7/KrzysztofJopekLab7ZadanieDomowe/Models/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace KrzysztofJopekLab7ZadanieDomowe.Models
+{
+    public class Book
+    {
+        [Key]
+        public int BookId { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        public int Year { get; set; }
+        [Required]
+        public string Type { get; set; }
+        [Required]
+        public string Description { get; set; }
+    }
+}
